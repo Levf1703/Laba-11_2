75fe2bbc5101d1a9c49374482f5aeaa6c8dc1042
zanko <zanko@CHUNGUK>
2024-04-15 00:24:55 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WinFormsApp_лаба_11_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            listBox1 = new ListBox();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button3 = new Button();
+            listBox2 = new ListBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.Location = new Point(12, 387);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 51);
+            button1.TabIndex = 0;
+            button1.Text = "Сгенерировать список";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(308, 344);
+            listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button2.Location = new Point(193, 387);
+            button2.Name = "button2";
+            button2.Size = new Size(159, 51);
+            button2.TabIndex = 2;
+            button2.Text = "Данные из файла";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(453, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(326, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(131, 47);
+            label1.TabIndex = 4;
+            label1.Text = "Введите город:";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button3.Location = new Point(351, 62);
+            button3.Name = "button3";
+            button3.Size = new Size(217, 83);
+            button3.TabIndex = 5;
+            button3.Text = "Показать рейсы и самолёты";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(598, 22);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(308, 344);
+            listBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(925, 444);
+            Controls.Add(listBox2);
+            Controls.Add(button3);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private Button button2;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button3;
+        private ListBox listBox2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+namespace WinFormsApp_лаба_11_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<AEROFLOT> time_table = new List<AEROFLOT>();
+        class AEROFLOT
+        {
+            public string city;
+            public string number;
+            public string plane;
+
+            public AEROFLOT(string city, string number, string plane)
+            {
+                this.city = city;
+                this.number = number;
+                this.plane = plane;
+            }
+            public string WhatCity()
+            {
+                return city;
+            }
+            public string Print()
+            {
+                return $"{city} {number} {plane}";
+            }
+            public string Print(string town)
+            {
+                return $"{number} {plane}";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] cities = { "Сочи", "Москва", "Красноярск", "Владивосток", "Стамбул", "Кемерово" };
+            string[] numbers = { "45F664", "24H74H", "63HJ48", "48HBX9", "238JBH92" };
+            string[] planes = { "Boeing 737", "Boeing 777", "Airbus А319", "Airbus А320", "Superjet 100" };
+            Random random = new Random();
+            time_table.Add(new AEROFLOT(cities[random.Next(0, 6)], numbers[random.Next(0, 5)], planes[random.Next(0, 5)]));
+            time_table.Add(new AEROFLOT(cities[random.Next(0, 6)], numbers[random.Next(0, 5)], planes[random.Next(0, 5)]));
+            time_table.Add(new AEROFLOT(cities[random.Next(0, 6)], numbers[random.Next(0, 5)], planes[random.Next(0, 5)]));
+            time_table.Add(new AEROFLOT(cities[random.Next(0, 6)], numbers[random.Next(0, 5)], planes[random.Next(0, 5)]));
+            time_table.Add(new AEROFLOT(cities[random.Next(0, 6)], numbers[random.Next(0, 5)], planes[random.Next(0, 5)]));
+            time_table.Add(new AEROFLOT(cities[random.Next(0, 6)], numbers[random.Next(0, 5)], planes[random.Next(0, 5)]));
+            time_table.Add(new AEROFLOT(cities[random.Next(0, 6)], numbers[random.Next(0, 5)], planes[random.Next(0, 5)]));
+            time_table.Add(new AEROFLOT(cities[random.Next(0, 6)], numbers[random.Next(0, 5)], planes[random.Next(0, 5)]));
+            time_table.Add(new AEROFLOT(cities[random.Next(0, 6)], numbers[random.Next(0, 5)], planes[random.Next(0, 5)]));
+            time_table.Add(new AEROFLOT(cities[random.Next(0, 6)], numbers[random.Next(0, 5)], planes[random.Next(0, 5)]));
+            for (int i = 0; i < time_table.Count; i++)
+                listBox1.Items.Add(time_table[i].Print());
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\zanko\\Desktop\\AEROFLOT.txt", false))
+            {
+                for (int i = 0; i < time_table.Count; i++)
+                    writer.WriteLine(time_table[i].Print());
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count != 0)
+            {
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                    listBox1.Items.Clear();
+            }
+            if (time_table.Count != 0)
+                time_table.Clear();
+            using (StreamReader reader = new StreamReader("C:\\Users\\zanko\\Desktop\\AEROFLOT.txt"))
+            {
+                string content = reader.ReadToEnd();
+                string[] str_sentens = content.Split('\n');
+                for (int k = 0; k < str_sentens.Length -1; k++)
+                {
+                    string[] zabiv = str_sentens[k].Split(' ');
+                    time_table.Add(new AEROFLOT(zabiv[0], zabiv[1], $"{zabiv[2]} {zabiv[3]}"));
+                }
+                foreach (string str in str_sentens)
+                    listBox1.Items.Add(str);
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox2.Items.Count; i++)
+                listBox2.Items.Clear();
+            byte count = 0;
+            if (textBox1.Text != null)
+            {
+                for (int j = 0; j < time_table.Count; j++)
+                {
+                    if (textBox1.Text == time_table[j].WhatCity())
+                    { listBox2.Items.Add(time_table[j].Print(textBox1.Text)); count = 1; }
+                }
+                if (count != 1)
+                    listBox2.Items.Add("Таких рейсов нет! Выберете другой город");
+            }
+        }
+    }
+}
